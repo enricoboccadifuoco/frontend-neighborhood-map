34e888231918ce50aff3ec391c10d5b3e2d96509
Enrico Boccadifuoco <enrico@mosaicoon.com>
2015-08-02 23:22:53 +0200

    First commit

---
+++ js/model/place.js
@@ -0,0 +1,51 @@
+// model
+var Place = function(place) {
+	var self = this;
+
+    self.name = place.name;
+    self.marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    self.nyTimes = [];
+    self.wikipedia = [];
+
+    self.populate = function() {
+        var deferred = $.Deferred();
+
+        $.when(
+            nyTimesService(self.name),
+            wikiSerivice(self.name)
+        ).then(function(response, response2) {
+            articles = response[0].response.docs;
+
+            for (var i = 0; i < articles.length; i++) {
+                var article = articles[i];
+
+                self.nyTimes.push({
+                    url: article.web_url,
+                    title: article.headline.main,
+                    text: article.snippet
+                });
+            };
+
+            var articleList = response2[0][1];
+
+            articleList.forEach(function(article) {
+                var url = "http://en.wikipedia.org/wiki/" + article;
+                self.wikipedia.push({
+                    url: url,
+                    title: article
+                });
+            });
+
+            deferred.resolve(self);
+
+        });
+
+        return deferred;
+    }
+
+    return self;
+};
---
+++ js/services/services.js
@@ -0,0 +1,22 @@
+var nyTimesService = function(search) {
+
+    if (search) {
+        var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + search + '&page=0&sort=newest&api-key=5b283515d351e3a4795aa7be7d9455cb:18:72435683';
+        return $.getJSON(nytimesUrl);
+    }
+
+    return false;
+}
+
+var wikiSerivice = function(search) {
+
+    if (search) {
+        var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&callback=wikiCallback&format=json";
+
+        return $.ajax(wikiUrl, {
+            dataType: "jsonp"
+        });
+    }
+
+    return false;
+}
